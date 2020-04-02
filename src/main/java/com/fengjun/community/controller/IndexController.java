4521827ab5988a4f2e954b9893fd6765e1ca5857
fengzmz <13618380176@163.com>
2020-04-02 22:35:46 +0800

    22

---
+++ src/main/java/com/fengjun/community/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.fengjun.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        System.out.println("进入index");
+        return "forward:/index";
+    }
+}
